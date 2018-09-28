db248939460b799db8bf5294631c62ca5c16ced9
nish7898 <32949526+nish7898@users.noreply.github.com>
2018-09-28 11:12:42 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'        : '775119562837171', // your App ID
-        'clientSecret'    : 'd911c32bcd34f50a2aa5755b83e7ead6', // your App Secret
+        'clientID'        : 'your client key', // your App ID
+        'clientSecret'    : 'your client secret', // your App Secret
         'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields'   : ['id', 'email', 'name'] // For requesting permissions from Facebook API
@@ -19,8 +19,8 @@ module.exports = {
     },
 
     'googleAuth' : {
-        'clientID'         : '546290068998-5ci61roaiiijk4055u5bbhta99bp8kqd.apps.googleusercontent.com',
-        'clientSecret'     : 'Kn2YMbGWD86T8pAzKSovN2Fl',
+        'clientID'         : 'your client ID',
+        'clientSecret'     : 'your client secret',
         'callbackURL'      : 'http://localhost:8080/auth/google/callback'
     }
 
